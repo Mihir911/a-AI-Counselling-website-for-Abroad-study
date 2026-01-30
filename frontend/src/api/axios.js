75bf618bd7888bbd4eecab8c27dd7f9d5b121df6
Mihir911 <spygamer737@gmail.com.com>
2026-01-30 14:33:36 +0530

    second commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://a-ai-counselling-website-for-abroad-study.onrender.com'
 });
 
 // Add token to requests
